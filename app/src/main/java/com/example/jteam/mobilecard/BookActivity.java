fbfced03d8b3299eb4fbe4c6584a162279efe750
Minsu9539 <operat1121@gmail.com>
2018-12-04 18:24:41 +0900

    ㅋㅁ

--- app/src/main/java/com/example/jteam/mobilecard/BookActivity.java
+++ app/src/main/java/com/example/jteam/mobilecard/BookActivity.java
@@ -1,10 +1,8 @@
-package com.example.jteam.mobilecard.library;
+package com.example.jteam.mobilecard;
 
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import com.example.jteam.mobilecard.R;
 
 public class BookActivity extends AppCompatActivity {
 
